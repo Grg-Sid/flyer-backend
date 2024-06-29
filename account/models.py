a24353763d5d5eda86a7cb5074477303f203c149
Grg-Sid <siddhantgurung007@gmail.com>
2024-06-29 12:49:20 +0530

    feat: add user model and auth

---
+++ account/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
+
+
+class CustomUser(AbstractUser):
+    objects = UserManager()
+
+    username = None
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
---
+++ account/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "email", "name", "password"]
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            email=validated_data["email"], name=validated_data["name"]
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
